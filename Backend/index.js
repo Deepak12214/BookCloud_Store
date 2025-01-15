95bcdbb9092ee248bc902e153050d465a576470c
Deepak12214 <dk8864820@gmail.com>
2025-01-16 03:46:59 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -37,6 +37,6 @@ app.use('/post', postRoutes);
 app.get('/', (req, res) => {
   res.send('hello')
 });
-app.listen(9000,()=>{
+app.listen(3000,()=>{
     console.log(`app listening on port ${port}`);
 });
